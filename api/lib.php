c33c7d18a7caa97891bf53cda0b719162c1a38ae
JacobAvila <jacobavila@gmail.com>
2024-02-15 23:13:10 -0600

    Commit Casa

--- api/lib.php
+++ api/lib.php
@@ -8,12 +8,3 @@ $user = $_SESSION['usuario'];
 if($user === NULL){
     header("Location: ".$home);
 }
-<?php
-session_start();
-include($home."api/modelo/model.php");
-
-$user = $_SESSION['usuario'];
-
-if($user === NULL){
-    header("Location: ".$home);
-}
